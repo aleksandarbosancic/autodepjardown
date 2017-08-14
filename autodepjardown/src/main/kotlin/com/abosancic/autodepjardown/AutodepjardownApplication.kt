9590b99893fbb67de807c8af21ab56d49c6ddd61
Aleksandar Bosancic <aco.bosancic@gmail.com>
2017-08-14 12:15:25 +0200

    init project

---
+++ autodepjardown/src/main/kotlin/com/abosancic/autodepjardown/AutodepjardownApplication.kt
@@ -0,0 +1,11 @@
+package com.abosancic.autodepjardown
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AutodepjardownApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(AutodepjardownApplication::class.java, *args)
+}
